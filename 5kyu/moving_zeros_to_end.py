5c1938b1bf0b843e8ff8879fbd009296e9971e8a
Rodrigo Rojo <rodrigo-rojo@outlook.com>
2022-05-04 00:03:17 -0500

    Create moving_zeros_to_end.py

---
+++ 5kyu/moving_zeros_to_end.py
@@ -0,0 +1,10 @@
+"""
+Write an algorithm that takes an array and moves all of the zeros to the end, preserving the order of the other elements.
+
+move_zeros([1, 0, 1, 2, 0, 1, 3]) # returns [1, 1, 2, 1, 3, 0, 0]
+"""
+
+
+def move_zeros(array):
+  array.sort()
+  return [i for i in array if i != 0] + [i for i in array if i == 0]
